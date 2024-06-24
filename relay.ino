5906b246297b90abe567a7cb7fae7c746a890357
elcanbag <63236691+elcanbag@users.noreply.github.com>
2024-06-24 22:30:08 +0400

    Create relay.ino

---
+++ relay.ino
@@ -0,0 +1,12 @@
+int relayPin = 7;
+
+void setup() {
+  pinMode(relayPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(relayPin, HIGH);
+  delay(1000);
+  digitalWrite(relayPin, LOW);
+  delay(1000);
+}
